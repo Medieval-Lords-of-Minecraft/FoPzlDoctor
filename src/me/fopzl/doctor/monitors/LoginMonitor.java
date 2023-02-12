2fe1eb86564a3899b9230b4e41219ab8911fcd99
FoPzl <officialsuperalex11@gmail.com>
2023-02-12 13:40:59 -0500

    Temp debug

--- src/me/fopzl/doctor/monitors/LoginMonitor.java
+++ src/me/fopzl/doctor/monitors/LoginMonitor.java
@@ -24,17 +24,17 @@ public class LoginMonitor extends Monitor {
 	public static Set<UUID> uniquePlayersDay = new HashSet<UUID>();
 	public static Set<UUID> uniquePlayersWeek = new HashSet<UUID>();
 	public static Set<UUID> uniquePlayersMonth = new HashSet<UUID>();
-	
+
 	// value item0 is newbie count, item1 is regular player count
 	// (newbies are identified by having joined within the past month)
 	public static Map<Rank, Pair<Integer, Integer>> uniqueLoginCountsDay = new HashMap<Rank, Pair<Integer, Integer>>();
 	public static Map<Rank, Pair<Integer, Integer>> uniqueLoginCountsWeek = new HashMap<Rank, Pair<Integer, Integer>>();
 	public static Map<Rank, Pair<Integer, Integer>> uniqueLoginCountsMonth = new HashMap<Rank, Pair<Integer, Integer>>();
-	
+
 	public LoginMonitor(ScheduleInterval i) {
 		super(i);
 		assert i == ScheduleInterval.DAILY; // :)
-
+		
 		dataFields.add("uniquePlayersDay");
 		dataFields.add("uniquePlayersWeek");
 		dataFields.add("uniquePlayersMonth");
@@ -42,18 +42,34 @@ public class LoginMonitor extends Monitor {
 		dataFields.add("uniqueLoginCountsWeek");
 		dataFields.add("uniqueLoginCountsMonth");
 	}
-	
+
+	// temp debug
+	@Override
+	protected void saveData(boolean async) {
+		Bukkit.getLogger().info("[FoPzlDoctor] Saving loginMonitor data (uniqueDay size=" + uniquePlayersDay.size() + ")");
+
+		super.saveData(async);
+	}
+
+	// temp debug
+	@Override
+	protected void loadData() {
+		super.loadData();
+		
+		Bukkit.getLogger().info("[FoPzlDoctor] Loaded loginMonitor data (uniqueDay size=" + uniquePlayersDay.size() + ")");
+	}
+
 	@Override
 	protected void update() {
 		LocalDateTime now = LocalDateTime.now();
-		
+
 		updateDay();
 		if (now.getDayOfWeek() == DayOfWeek.SUNDAY)
 			updateWeek();
 		if (now.getDayOfMonth() == 1)
 			updateMonth();
 	}
-	
+
 	private void updateDay() {
 		List<String> sqls = new ArrayList<String>();
 		String server = NeoCore.getInstanceKey();
@@ -62,7 +78,7 @@ public class LoginMonitor extends Monitor {
 			String rank = entry.getKey().toString();
 			int noobCount = entry.getValue().getValue0();
 			int regularCount = entry.getValue().getValue1();
-			
+
 			sqls.add(
 					"insert into fopzldoctor_loginMonitor (server, period, rank, noobCount, regularCount) values ('" + server + "', '" + period + "', '" + rank
 							+ "', " + noobCount + ", " + regularCount + ");"
@@ -71,7 +87,7 @@ public class LoginMonitor extends Monitor {
 		permSaveData(sqls);
 		resetDay();
 	}
-	
+
 	private void updateWeek() {
 		List<String> sqls = new ArrayList<String>();
 		String server = NeoCore.getInstanceKey();
@@ -80,7 +96,7 @@ public class LoginMonitor extends Monitor {
 			String rank = entry.getKey().toString();
 			int noobCount = entry.getValue().getValue0();
 			int regularCount = entry.getValue().getValue1();
-			
+
 			sqls.add(
 					"insert into fopzldoctor_loginMonitor (server, period, rank, noobCount, regularCount) values ('" + server + "', '" + period + "', '" + rank
 							+ "', " + noobCount + ", " + regularCount + ");"
@@ -89,7 +105,7 @@ public class LoginMonitor extends Monitor {
 		permSaveData(sqls);
 		resetWeek();
 	}
-	
+
 	private void updateMonth() {
 		List<String> sqls = new ArrayList<String>();
 		String server = NeoCore.getInstanceKey();
@@ -98,7 +114,7 @@ public class LoginMonitor extends Monitor {
 			String rank = entry.getKey().toString();
 			int noobCount = entry.getValue().getValue0();
 			int regularCount = entry.getValue().getValue1();
-			
+
 			sqls.add(
 					"insert into fopzldoctor_loginMonitor (server, period, rank, noobCount, regularCount) values ('" + server + "', '" + period + "', '" + rank
 							+ "', " + noobCount + ", " + regularCount + ");"
@@ -107,38 +123,38 @@ public class LoginMonitor extends Monitor {
 		permSaveData(sqls);
 		resetMonth();
 	}
-	
+
 	private static void resetDay() {
 		uniquePlayersDay.clear();
 		uniqueLoginCountsDay.clear();
 	}
-	
+
 	private static void resetWeek() {
 		uniquePlayersDay.clear();
 		uniqueLoginCountsDay.clear();
 	}
-	
+
 	private static void resetMonth() {
 		uniquePlayersDay.clear();
 		uniqueLoginCountsDay.clear();
 	}
-	
+
 	public static void tryIncPlayer(UUID uuid) {
 		tryIncPlayer(uuid, uniquePlayersDay, uniqueLoginCountsDay);
 		tryIncPlayer(uuid, uniquePlayersWeek, uniqueLoginCountsWeek);
 		tryIncPlayer(uuid, uniquePlayersMonth, uniqueLoginCountsMonth);
 	}
-	
+
 	private static void tryIncPlayer(UUID uuid, Set<UUID> uniquePlayers, Map<Rank, Pair<Integer, Integer>> uniqueLoginCounts) {
 		if (uniquePlayers.contains(uuid))
 			return;
-		
+
 		Player p = Bukkit.getPlayer(uuid);
 		Rank rank = Rank.getPlayerRank(p);
-		
+
 		LocalDateTime firstPlayed = LocalDateTime.ofEpochSecond(p.getFirstPlayed(), 0, ZoneOffset.UTC);
 		boolean noob = firstPlayed.isAfter(LocalDateTime.now().minusMonths(1));
-		
+
 		Pair<Integer, Integer> pair = uniqueLoginCounts.getOrDefault(rank, Pair.with(0, 0));
 		if (noob) {
 			pair.setAt0(pair.getValue0() + 1);
